c61f31b17a049099b27c0adada952483887ce7b0
Leonard Becker <80252081+Jolumine@users.noreply.github.com>
2023-01-09 21:41:13 +0100

    New Version 0.0.2

--- matrops/__init__.py
+++ matrops/__init__.py
@@ -4,7 +4,7 @@
 # and is released under the "MIT License Agreement". Please see the LICENSE
 # file that should have been included as part of this package.
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __name__ = "matrops"
 __license__ = "MIT"
 __information__ = "README.md"
